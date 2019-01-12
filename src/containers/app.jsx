4ab9307b792631f7ba463bf47d04d2a8c9a05354
name <1409697119@qq.com>
2019-01-12 08:44:17 +0800

    frame

---
+++ src/containers/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'common/css/index.css'
+class App extends React.Component{
+    render(){
+        return <div>
+            hello react
+        </div>
+    }
+}
+
+export default App;
