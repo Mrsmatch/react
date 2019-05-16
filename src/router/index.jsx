b41edb1b2a9c80424e99d2369f3d76a4bba3a3e8
admin@admin-pc <1409697119@qq.com>
2019-05-16 14:59:26 +0800

    new

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Map from './map'
 import Routers from './router'
-class Rindex extends React.Component{
-    render(){
-        const {routers}=this.props;
-        return <div>
-            <Map routers={routers===undefined?Routers:routers}></Map>
+class Rindex extends React.Component {
+    render() {
+        const { routers } = this.props;
+        return <div >
+            <Map routers = { routers } > </Map>
         </div>
     }
 }
--- src/router/map.jsx
+++ src/router/map.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import {Route,Switch,Redirect} from 'react-router-dom'
-class Map extends React.Component{
-    render(){
-        const {routers}=this.props;
-        
-        return <Switch>
-            {
-               
-            }
-        </Switch>
+import { Route, Switch, Redirect } from 'react-router-dom'
+class Map extends React.Component {
+    render() {
+        const { routers } = this.props;
+
+        return <Switch > {
+                新内容
+            } 
+            </Switch>
     }
 }
 export default Map;
